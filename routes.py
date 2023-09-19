6d29e35ae1c4185377c06ed5724422561b2dd339
mikaelri <mikael.rintala@outlook.com>
2023-09-19 16:40:25 +0300

    add layout, update html templates, update routes and users

--- routes.py
+++ routes.py
@@ -1,26 +1,59 @@
 """module app to run the app.py file and flask used to handle the routing of the application"""
 from flask import render_template, redirect, request, session
 from app import app
+import users
 
 #Front page of the application
 @app.route('/')
 def index():
     """Function creating bullet points in front page"""
     words = [
-        'create your own budgets',
-        'add income & expense transactions by categories',
-        'search transactions by category & word'
+        "create your own budgets",
+        "add income & expense transactions by categories",
+        "search transactions by category & word"
         ]
     reasons = [
-        'application is free of charge',
-        'bugeting made simple',
-        'personal savings for the future', 
-        'better visibility on your financials', 
-        'much more - register now and try!'
+        "application is free of charge",
+        "bugeting made simple",
+        "personal savings for the future", 
+        "better visibility on your financials", 
+        "much more - register now and try!"
         ]
     return render_template('index.html', items1= words, items2=reasons)
 
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route("/register", methods=["GET", "POST"])
+def create_user():
+    """Function handling when a new user is created"""
+
+    if request.method == "GET":
+        return render_template("register.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        if len(username) < 1 or len(username) > 25:
+            return render_template("error.html", message="Username should be between 1-25 characters.")
+        
+        password1 = request.form["password1"]
+        password2 = request.form ["password2"]
+        if len(password1) < 1 or len(password1) > 25:
+            return render_template("error.html", message="Password should be between 1-25 characters.")
+        if password1 != password2:
+            return render_template("error.html", message="Given passwords are not the same")
+        if password1 == "":
+            return render_template("error.html", message="Password is empty")
+        
+        role = request.form["role"]
+        if role not in ("1", "2"):
+            return render_template("error.html", message="Unknown user type")
+        
+        if not users.create_user(username, password1, role):
+            return render_template("error.html", message="Registration not succesfull, check username and password")
+        return redirect("/register")
+    
+    return render_template("register.html")
+
+
+@app.route("/login", methods = ["GET", "POST"])
 def login():
     """Function handling the log-in"""
     if request.method == "GET":
@@ -28,19 +61,14 @@ def login():
     
     if request.method == "POST":
         username = request.form["username"]
-        password = request.form["password"]
-        # TO DO: check username and password
-    session['username'] = username
-    return render_template("login.html")
+        password = request.form["password1"]
 
-@app.route('/logout')
+    if not users.login(username, password):
+        return render_template("error.html", message="Wrong username or password")
+    return redirect("/")
+
+@app.route("/logout")
 def logout():
     """Function to log out from your personal pages"""
-    del session['username']
-    return redirect('/')
-
-
-@app.route('/register', methods = ["GET"])
-def register():
-    """Function to route to the registration page for sign-up"""
-    return render_template("register.html")
+    users.logout()
+    return redirect("/")
--- users.py
+++ users.py
@@ -6,10 +6,47 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 def login(username, password):
     """login function for database"""
+    sql = "SELECT password, id, role FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if not user:
+        return False
+    if not check_password_hash(user[0], password):
+        return False
+    session["user_id"] = user[1]
+    session["user_username"] = username
+    session["user_role"] = user[2]
+    session["csrf_token"] = os.urandom(16).hex()
+    return True
 
-def register(username, password, role):
+def logout():
+    """log-out function for database"""
+    del session["user_id"]
+    del session["user_username"]
+    del session["user_role"]
+
+def create_user(username, password, role):
     """function for registering a new user to database"""
     hash_value = generate_password_hash(password)
+    try:
+        sql = """INSERT INTO users (username, password, role) 
+        VALUES (:username, :password, :role)"""
+        db.session.execute(sql, {"username":username, "password":hash_value, "role":role})
+        db.session.commit()
+    except:
+        return False
+    return login(username, password)
+
+def user_id():
+    return session.get("user_id", 0)
+
+def require_role(role):
+    if role > session.get("user_role", 0):
+        abort(403)
+
+def check_csrf():
+    if session["csrf_token"] != request.form["csrf_token"]:
+        abort(403)
 
 
 
