236ab35337da9faa61a2f74b87eadbfd689c38a0
mikaelri <mikael.rintala@outlook.com>
2023-09-20 21:08:20 +0300

    fixes for typing

--- schema.sql
+++ schema.sql
@@ -1,6 +1,22 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username TEXT, 
     password TEXT,
     role INTEGER,
 );
+
+CREATE TABLE IF NOT EXISTS budgets (
+    id SERIAL PRIMARY KEY,
+    creator_id INTEGER REFERENCES users(id),
+    name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id SERIAL PRIMARY KEY,
+    budget_id INTEGER REFERENCES budgets,
+    income INTEGER,
+    expense INTEGER,
+    category TEXT,
+    message TEXT,
+);
+    
