628c1b0ab82e1db7cb301b1e95c4c9c31b4e9523
mikaelri <mikael.rintala@outlook.com>
2023-09-21 16:45:17 +0300

    small fix for typing

--- db.py
+++ db.py
@@ -4,7 +4,6 @@ from os import getenv
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 
-
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 # End-of-file (EOF)
