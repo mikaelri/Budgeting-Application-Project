7f1fa90aeb585282310b878fb98ae213152340f6
mikaelri <mikael.rintala@outlook.com>
2023-09-28 18:20:55 +0300

    add template, fix that same name cannot be used in budget

--- budgets.py
+++ budgets.py
@@ -25,54 +25,19 @@ def new_budget(name, creator_id, income, expense, income_category, expense_categ
         return True
     except:
         return False
-    
-def add_transaction(income, expense, income_category, expense_category, message):
-    """function to add transactions continously to selected budget"""
+   
+def budget_exists(name):
+    """function to check if username exists"""
+    sql = text("SELECT name from budgets WHERE name=:name")
+    result = db.session.execute(sql, {"name": name})
+    existing_budget = result.fetchone()
 
-    try:
-        if income == "":
-            income = None
-        if expense == "":
-            expense = None
-        if income_category == "":
-            income_category = None
-        if expense_category == "":
-            expense_category = None
-        
-        sql = text(
-            """"INSERT INTO budgets (income, expense, income_category, expense_category, message)
-            VALUES(:income, :expense, :income_category, :expense_category, :message)""")
-        db.session.execute(sql,{"income":income, "expense":expense, 
-                                "income_category":income_category, 
-                                "expense_category":expense_category, "message":message})
-        db.session.commit()
+    if existing_budget:
         return True
-    except:
-        return False
-    
-def calculate_net_result():
-    """function to calculate the net result and inserting it to results table"""
-    try:
-        sql = text(
-        """INSERT INTO results (budget_id, result)
-        SELECT b.id AS budget_id, SUM(b.income - b.expense) AS net_result
-        FROM budgets AS b
-        GROUP BY b.id""")
-        db.session.execute(sql)
-        db.session.commit()
-        return True
-    except:
+    else:
         return False
 
-def get_net_result(budget_id):
-    """function to retrieve the net result for a selected budget"""
-    
-    sql = text("SELECT result FROM results WHERE budget_id=:budget_id")
-    result = db.session.execute(sql, {"budget_id":budget_id}).scalar()
-    
-    return result
-
-def see_budgets(creator_id): 
+def see_budgets(creator_id):
     """function to view budgets"""
     sql = text("SELECT id, name FROM budgets WHERE creator_id=:creator_id")
     result = db.session.execute(sql, {"creator_id":creator_id})
@@ -92,3 +57,54 @@ def get_budget_id(budget_id):
     result = db.session.execute(sql, {"budget_id":budget_id})
     budget = result.fetchone()
     return budget
+
+def add_transaction(budget_id, income, expense, income_category, expense_category, message):
+    """function to add transactions continuously to selected budget"""
+
+    if income == "":
+        income = None
+    if expense == "":
+        expense = None
+    if income_category == "":
+        income_category = None
+    if expense_category == "":
+        expense_category = None
+    
+    sql = text(
+        """INSERT INTO transactions (budget_id, income, expense, income_category, expense_category, message)
+        VALUES(:budget_id, :income, :expense, :income_category, :expense_category, :message)""")
+    transaction = db.session.execute(sql, {
+                            "budget_id": budget_id,
+                            "income": income,
+                            "expense": expense,
+                            "income_category": income_category,
+                            "expense_category": expense_category,
+                            "message": message
+                            })
+    db.session.commit()
+    return transaction
+
+def calculate_net_result():
+    """function to calculate the net result and updating it to budgets table"""
+    try:
+        sql_update_result = text(
+            """
+            UPDATE budgets
+            SET result = SUM(income, 0) - COALESCE(expense, 0);
+            """
+        )
+        db.session.execute(sql_update_result)
+        db.session.commit()
+
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+def get_net_result(budget_id):
+    """function to retrieve the net result for a selected budget"""
+    
+    sql = text("SELECT result FROM results WHERE budget_id=:budget_id")
+    result = db.session.execute(sql, {"budget_id":budget_id}).scalar()
+    
+    return result
