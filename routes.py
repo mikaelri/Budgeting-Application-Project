885e5f34c7d01429250d067a0f8cfa269bb12770
mikaelri <mikael.rintala@outlook.com>
2023-10-07 15:06:18 +0300

    add delete function for admins and update add transactions

--- routes.py
+++ routes.py
@@ -13,7 +13,6 @@ def index():
 @app.route("/register", methods=["GET", "POST"])
 def create_user():
     """Function handling when a new user is created"""
-
     if request.method == "GET":
         return render_template("register.html")
 
@@ -89,7 +88,7 @@ def create_new_budget():
         budget_count = budgets.get_budget_count(creator_id)
 
         if budgets.budget_exists(name):
-            flash("The name of the budget already exists, create a new one", "error")
+            flash("The name of the budget already exists, create a new one.", "error")
             return redirect("/budget")
 
         if budget_count >= 5:
@@ -123,17 +122,22 @@ def select_budget():
     budget_id = request.args.get("budget_id")
     selected_budget = budgets.get_budget_id(budget_id)
 
-    return render_template("transactions.html", selected_budget=selected_budget, 
-                                                budget_id=budget_id)
+    return render_template("transactions.html", selected_budget=selected_budget, budget_id=budget_id)
 
 @app.route("/transactions/<int:budget_id>", methods=["GET", "POST"])
-def add_new_transactions(budget_id):
+def add_new_transactions(budget_id: int):
     """function to add continously transactions to a selected budget"""
 
+    session['budget_id'] = budget_id
+
     if request.method == "GET":
+        budget_id = request.args.get("budget_id")
+        if budget_id:
+            session['budget_id'] = budget_id
         return render_template("transactions.html")
 
     if request.method == "POST":
+        budget_id = session.get('budget_id')
         income = request.form.get("income")
         expense = request.form.get("expense")
         income_category = request.form.get("income_category")
@@ -143,11 +147,11 @@ def add_new_transactions(budget_id):
         if userbudgets.add_transaction(budget_id, income, expense, income_category, 
                                        expense_category, message):
             flash("Transaction added succesfully!", "success")
-            return redirect("/transactions")
+            return redirect(f"/transactions?budget_id={session.get('budget_id')}")
         else:
             flash("Failed to add transactions to the budget, try again!")
-            return redirect("/transactions")
-
+            return redirect(f"/transactions?budget_id={session.get('budget_id')}")
+        
     return render_template("transactions.html", budget_id=budget_id)
 
 @app.route("/netresult", methods=["GET"])
@@ -161,21 +165,35 @@ def view_net_result():
     net_result = userbudgets.calculate_net_result(budget_id)
 
     return render_template("netresult.html", 
-                           selected_budget=select_budget, 
-                           budget_id=budget_id,
-                           net_result=net_result)
+                           selected_budget=select_budget, budget_id=budget_id, net_result=net_result)
 
-@app.route("/userlist", methods=["GET", "POST"])
-def userlist():
-    """Function to route to admin user page and functionalities"""
+@app.route("/admin", methods=["GET", "POST"])
+def admin_list():
+    """Route to admin user page and functionalities"""
     if request.method == "GET":
         user_id = session.get("user_id")
         user_role = users.get_user_role(user_id)
         if user_role and user_role == 2:
             all_users = users.get_user_list()
-            return render_template("userlist.html", users=all_users)
+            all_budgets = userbudgets.get_all_budgets() 
+            return render_template("admin.html", users=all_users, budgets=all_budgets)
         else:
-            flash("Access denied, only for admin users")
+            flash("Access denied, only for admin users", "error")
             return redirect("/login")
+           
+    """Remove existing budget if there is at least 1 budget created"""
+    if request.method == "POST":
+        creator_id = session.get("user_id")
+        budget_count = budgets.get_budget_count(creator_id)
 
+        if budget_count < 1:
+            flash ("No budgets to remove.", "error")
+            return redirect("/admin")
+
+        budget_id = request.form.get("budget_id")
+        success = userbudgets.delete_budget(budget_id)
+
+        if success:
+            flash ("Budget was removed succesfully!", "success")
+            return redirect("/admin")
 
