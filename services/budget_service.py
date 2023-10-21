3fdfd5d86b522f05182cdeaf3621e5e636619df1
mikaelri <mikael.rintala@outlook.com>
2023-10-21 14:55:56 +0300

    align flash messages body

--- services/budget_service.py
+++ services/budget_service.py
@@ -13,15 +13,15 @@ def process_fields(income, expense, income_category, expense_category, message):
 def validate_fields(income, expense, income_category, expense_category):
     # Function to validate fields in the add transaction form
     if not income and not expense:
-        return False, "Failed to submit the transaction, either income or expense has to be added."
+        return False, "Failed to add transaction. Income or expense has to be added."
 
     elif (income and float(income) < 0) or (expense and float(expense) < 0):
-        return False, "Negative values cannot be used when adding transactions."
+        return False, "Failed to add transaction. Negative values cannot be used."
     
     elif (income and float(income) > 2147483647) or (expense and float(expense) > 2147483647):
         return False, "Failed to add transaction. The entered amount exceeds the maximum limit of 2 147 483 647."
     
     elif (income and not income_category) or (expense and not expense_category):
-        return False, "Failed to submit the transaction, please select income and/or expense and related categories."
+        return False, "Failed to add transaction. Select income and/or expense and related categories."
     
     return True, ""
